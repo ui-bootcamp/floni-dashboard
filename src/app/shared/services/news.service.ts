1283d072d27705b0ffa886bb96673325ea58b7ce
niha6220 <nils.haeusler@esolutions.de>
2020-01-23 16:27:43 +0100

    feat: basic news module (front- and backend)

--- src/app/shared/services/news.service.ts
+++ src/app/shared/services/news.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Article } from '../models/article.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private URL = 'http://localhost:3000/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public getArticles(): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(`${this.URL}news`);
+  }
 }
