74e634be5cfc7592164d3fe93aaa86f51cdca8c3
flwa6204 <florian.warnke@esolutions.de>
2020-01-23 15:15:46 +0100

    feat: added media backend (search missing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, DashboardModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DashboardModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/models/track.model.ts
@@ -0,0 +1,9 @@
+export interface Track {
+  id: number;
+  albumId: number;
+  artistId: number;
+  title: string;
+  duration: number;
+  createdAt: string;
+  updatedAt: string;
+}
