7ad75886f38d5bd0ce8c3e277ba690f792dc2e87
niha6220 <nils.haeusler@esolutions.de>
2020-01-23 16:27:43 +0100

    feat: basic news module (front- and backend)

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { DashboardComponent } from './dashboard.component';
+import { NewsModule } from './news/news.module';
 
 @NgModule({
   declarations: [SearchComponent, DashboardComponent],
-  imports: [CommonModule, MatSlideToggleModule],
+  imports: [CommonModule, MatSlideToggleModule, NewsModule],
   exports: [DashboardComponent]
 })
 export class DashboardModule {}
--- src/app/dashboard/news/news.module.ts
+++ src/app/dashboard/news/news.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article/article.component';
-
-
+import { NewsComponent } from './news.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule, MatListModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
-  declarations: [ArticleComponent],
+  declarations: [ArticleComponent, NewsComponent],
+  exports: [NewsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatMenuModule,
+    MatListModule
   ]
 })
-export class NewsModule { }
+export class NewsModule {}
