07ac0a0439bc57539657fbcd73988618eab58e75
florianwarnkeeso <59647394+florianwarnkeeso@users.noreply.github.com>
2020-01-27 12:59:18 +0100

    Feature/flwa6204/search service (#4)
    feat: add search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ThemeService } from './shared/services/theme.service';
 import { Observable } from 'rxjs';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'db-root',
@@ -11,7 +12,17 @@ export class AppComponent {
   public title = 'dashboard';
   public isDarkTheme$: Observable<boolean>;
 
-  constructor(private themeService: ThemeService) {
+  constructor(
+    private themeService: ThemeService,
+    overlayContainer: OverlayContainer
+  ) {
     this.isDarkTheme$ = this.themeService.isDarkTheme$;
+    this.isDarkTheme$.subscribe((value: boolean) => {
+      if (value) {
+        overlayContainer.getContainerElement().classList.add('dark-theme');
+      } else {
+        overlayContainer.getContainerElement().classList.remove('dark-theme');
+      }
+    });
   }
 }
--- src/app/shared/services/media.service.ts
+++ src/app/shared/services/media.service.ts
@@ -1,47 +1,145 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Artist } from '../models/artist.model';
 import { Album } from '../models/album.model';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Track } from '../models/track.model';
+import { SearchResult } from '../models/search-result.model';
+import { SearchResultType } from '../models/search-result-type.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaService {
-  private mediaURL = 'http://localhost:3000/';
+  private artistsURL = 'http://localhost:3000/artists/';
+  private albumsURL = 'http://localhost:3000/albums/';
+  private tracksURL = 'http://localhost:3000/tracks/';
 
   constructor(private http: HttpClient) {}
 
   public getAllArtists(): Observable<Artist[]> {
-    const artistsURL = `${this.mediaURL}artists`;
-    return this.http.get<Artist[]>(artistsURL);
+    return this.http.get<Artist[]>(this.artistsURL);
+  }
+
+  public getAllArtistsWith(artistName: string): Observable<SearchResult[]> {
+    return this.http.get<Artist[]>(this.artistsURL).pipe(
+      map(artists =>
+        artists.filter(x =>
+          x.name.toUpperCase().includes(artistName.toUpperCase())
+        )
+      ),
+      map(artists =>
+        artists.map(
+          artist =>
+            new SearchResult(
+              artist.id,
+              artist.name,
+              '',
+              SearchResultType.Artist
+            )
+        )
+      )
+    );
   }
 
   public getArtist(id: number): Observable<Artist> {
-    const artistURL = `${this.mediaURL}artists/${id}`;
+    const artistURL = `${this.artistsURL}${id}`;
     return this.http.get<Artist>(artistURL);
   }
 
   public getAlbum(id: number): Observable<Album> {
-    const albumURL = `${this.mediaURL}albums/${id}`;
+    const albumURL = `${this.albumsURL}${id}`;
     return this.http.get<Album>(albumURL);
   }
 
+  public getAllAlbumsWith(albumName: string): Observable<SearchResult[]> {
+    return this.http.get<Album[]>(this.albumsURL).pipe(
+      map(albums =>
+        albums.filter(x =>
+          x.name.toUpperCase().includes(albumName.toUpperCase())
+        )
+      ),
+      map(albums => {
+        return albums.map(
+          album =>
+            new SearchResult(
+              album.id,
+              album.name,
+              album.artistId.toString(),
+              SearchResultType.Album
+            )
+        );
+      }),
+      switchMap(results => {
+        return combineLatest(
+          of(results),
+          this.http.get<Artist[]>(this.artistsURL)
+        );
+      }),
+      map(([results, artists]) => {
+        return results.map(result => {
+          const foundArtist = artists.find(
+            x => x.id.toString() === result.additionalInformation
+          );
+          if (foundArtist !== undefined) {
+            result.additionalInformation = foundArtist.name;
+          }
+          return result;
+        });
+      })
+    );
+  }
+
   public getAlbumsForArtist(artistId: number): Observable<Album[]> {
-    const albumURL = `${this.mediaURL}albums`;
     return this.http
-      .get<Album[]>(albumURL)
+      .get<Album[]>(this.albumsURL)
       .pipe(
         map(resultArray => resultArray.filter(x => x.artistId === artistId))
       );
   }
 
   public getTracksForAlbum(albumId: number): Observable<Track[]> {
-    const albumURL = `${this.mediaURL}tracks`;
     return this.http
-      .get<Track[]>(albumURL)
+      .get<Track[]>(this.tracksURL)
       .pipe(map(resultArray => resultArray.filter(x => x.albumId === albumId)));
+  }
+
+  public getAllTracksWith(trackName: string): Observable<SearchResult[]> {
+    return this.http.get<Track[]>(this.tracksURL).pipe(
+      map(tracks =>
+        tracks.filter(x =>
+          x.title.toUpperCase().includes(trackName.toUpperCase())
+        )
+      ),
+      map(tracks =>
+        tracks.map(
+          track =>
+            new SearchResult(
+              track.id,
+              track.title,
+              track.albumId.toString(),
+              SearchResultType.Track
+            )
+        )
+      ),
+      switchMap(results => {
+        return combineLatest(
+          of(results),
+          this.http.get<Album[]>(this.albumsURL)
+        );
+      }),
+      map(([results, albums]) => {
+        return results.map(result => {
+          const foundAlbum = albums.find(
+            x => x.id.toString() === result.additionalInformation
+          );
+          if (foundAlbum !== undefined) {
+            result.additionalInformation = foundAlbum.name;
+          }
+          return result;
+        });
+      })
+    );
   }
 }
