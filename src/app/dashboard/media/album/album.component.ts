6fb322d980f153bac2cf28452a11aec70b1b981d
niha6220 <nils.haeusler@esolutions.de>
2020-01-27 17:44:56 +0100

    feat: media colum view and fake player

--- src/app/dashboard/media/album/album.component.ts
+++ src/app/dashboard/media/album/album.component.ts
@@ -1,25 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Album } from '../../../shared/models/album.model';
-import { MediaService } from '../../../shared/services/media.service';
-import { Observable } from 'rxjs';
-import { Track } from '../../../shared/models/track.model';
 
 @Component({
   selector: 'db-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.scss']
 })
-export class AlbumComponent implements OnInit {
+export class AlbumComponent {
   @Input() public album: Album | undefined;
-  public tracks$: Observable<Track[]>;
-
-  constructor(private mediaService: MediaService) {
-    this.tracks$ = new Observable<Track[]>();
-  }
-
-  ngOnInit() {
-    if (this.album) {
-      this.tracks$ = this.mediaService.getTracksForAlbum(this.album.id);
-    }
-  }
 }
--- src/app/dashboard/media/artist/artist.component.ts
+++ src/app/dashboard/media/artist/artist.component.ts
@@ -1,25 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Artist } from '../../../shared/models/artist.model';
-import { Observable } from 'rxjs';
-import { Album } from '../../../shared/models/album.model';
-import { MediaService } from '../../../shared/services/media.service';
 
 @Component({
   selector: 'db-artist',
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.scss']
 })
-export class ArtistComponent implements OnInit {
+export class ArtistComponent {
   @Input() public artist: Artist | undefined;
-  public albums$: Observable<Album[]>;
-
-  constructor(private mediaService: MediaService) {
-    this.albums$ = new Observable<Album[]>();
-  }
-
-  ngOnInit() {
-    if (this.artist) {
-      this.albums$ = this.mediaService.getAlbumsForArtist(this.artist.id);
-    }
-  }
 }
