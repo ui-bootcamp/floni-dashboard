7351e0d230bcb9c8cc9ea2e1f4f6dd72c675ab6e
florianwarnkeeso <59647394+florianwarnkeeso@users.noreply.github.com>
2020-01-27 12:59:18 +0100

    Feature/flwa6204/search service (#4)
    feat: add search

--- src/app/dashboard/search/search.component.ts
+++ src/app/dashboard/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { SearchService } from '../../shared/services/search.service';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { SearchResult } from '../../shared/models/search-result.model';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'db-search',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  public searchField: FormControl = new FormControl();
+  public searchResults$: Observable<SearchResult[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private searchService: SearchService) {
+    this.searchResults$ = new Observable<SearchResult[]>();
   }
 
+  ngOnInit() {
+    this.searchResults$ = this.searchField.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((term: string) =>
+        term.length > 0 ? this.searchService.search(term) : of([])
+      )
+    );
+  }
 }
