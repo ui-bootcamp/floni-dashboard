e08388b0253fd0ddf79616b2f5aa43e492968a7a
niha6220 <nils.haeusler@esolutions.de>
2020-01-28 13:49:22 +0100

    feat: page navigation via the router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import 'hammerjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [DashboardModule]
+      imports: [DashboardModule, RouterTestingModule]
     }).compileComponents();
   }));
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,40 +1,30 @@
 import {
-  MatAutocompleteModule,
   MatIconModule,
-  MatInputModule,
   MatSlideToggleModule,
   MatButtonModule,
   MatToolbarModule
 } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { SearchComponent } from './search/search.component';
 import { DashboardComponent } from './dashboard.component';
-import { SearchResultTypeToIconPipe } from '../shared/pipes/search-result-type-to-icon.pipe';
 import { NewsModule } from './news/news.module';
 import { MediaModule } from './media/media.module';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
-  declarations: [
-    SearchComponent,
-    DashboardComponent,
-    SearchResultTypeToIconPipe
-  ],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     MatSlideToggleModule,
-    MatInputModule,
-    ReactiveFormsModule,
     MatIconModule,
-    MatAutocompleteModule,
+    MatToolbarModule,
+    MatButtonModule,
     NewsModule,
     MediaModule,
-    MatToolbarModule,
-    MatButtonModule
+    SearchModule
   ],
   exports: [DashboardComponent]
 })
