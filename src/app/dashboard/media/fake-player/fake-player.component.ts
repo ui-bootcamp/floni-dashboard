aa900942c498d1e89c506c10ab738d251d36b92c
niha6220 <nils.haeusler@esolutions.de>
2020-01-29 10:03:19 +0100

    feat: fake player supports now all relevant tasks

--- src/app/dashboard/media/fake-player/fake-player.component.ts
+++ src/app/dashboard/media/fake-player/fake-player.component.ts
@@ -8,4 +8,18 @@ import { Track } from '../../../shared/models/track.model';
 })
 export class FakePlayerComponent {
   @Input() track: Track | undefined;
+  public playPauseIcon = 'play_arrow';
+  public volumeIcon = 'volume_up';
+  public cover =
+    'https://e-cdns-images.dzcdn.net/images/cover/f14becbb0d888cd5457d18d3bb670731/56x56-000000-80-0-0.jpg';
+
+  public onPlayPauseClick(): void {
+    this.playPauseIcon =
+      this.playPauseIcon === 'pause' ? 'play_arrow' : 'pause';
+  }
+
+  public onVolumeClick(): void {
+    this.volumeIcon =
+      this.volumeIcon === 'volume_up' ? 'volume_off' : 'volume_up';
+  }
 }
