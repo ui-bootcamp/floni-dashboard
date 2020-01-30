356ddf786196bbae98a2e8ccbb19763dd66a19d6
florianwarnkeeso <59647394+florianwarnkeeso@users.noreply.github.com>
2020-01-30 11:58:17 +0100

    feat: added unit tests

--- src/app/shared/services/media.service.spec.ts
+++ src/app/shared/services/media.service.spec.ts
@@ -11,6 +11,83 @@ describe('MediaService', () => {
   let injector: TestBed;
   let httpMock: HttpTestingController;
   let service: MediaService;
+  const dummyTracks = [
+    {
+      id: 1,
+      albumId: 2,
+      artistId: 3,
+      title: 'the Title',
+      duration: 100,
+      createdAt: '1990-01-01',
+      updatedAt: '1990-01-01',
+      isFavorite: false
+    },
+    {
+      id: 2,
+      albumId: 77,
+      artistId: 3,
+      title: 'the Title2',
+      duration: 100,
+      createdAt: '1990-01-01',
+      updatedAt: '1990-01-01',
+      isFavorite: false
+    },
+    {
+      id: 3,
+      albumId: 77,
+      artistId: 3,
+      title: 'the Title3',
+      duration: 100,
+      createdAt: '1990-01-01',
+      updatedAt: '1990-01-01',
+      isFavorite: false
+    }
+  ];
+  const dummyArtists = [
+    {
+      name: 'Deichkind',
+      id: 2,
+      createdAt: '1900-01-01',
+      updatedAt: '1990-01-01',
+      isFavorite: false
+    },
+    {
+      name: 'McFitti',
+      id: 3,
+      createdAt: '1900-01-01',
+      updatedAt: '1990-01-01',
+      isFavorite: false
+    }
+  ];
+  const dummyAlbums = [
+    {
+      id: 1,
+      artistId: 1,
+      name: 'Album1',
+      cover: 'cover',
+      coverSmall: 'cover',
+      coverMedium: 'cover',
+      coverBig: 'cover',
+      coverXL: 'cover',
+      createdAt: 'cover',
+      updatedAt: 'cover',
+      isFavorite: false
+    },
+    {
+      id: 2,
+      artistId: 2,
+      name: 'Album2',
+      cover: 'cover',
+      coverSmall: 'cover',
+      coverMedium: 'cover',
+      coverBig: 'cover',
+      coverXL: 'cover',
+      createdAt: 'cover',
+      updatedAt: 'cover',
+      isFavorite: false
+    }
+  ];
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -23,23 +100,6 @@ describe('MediaService', () => {
 
   describe('getAllArtists', () => {
     it('returning all artists', done => {
-      const dummyArtists = [
-        {
-          name: 'Deichkind',
-          id: 2,
-          createdAt: '1900-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        },
-        {
-          name: 'McFitti',
-          id: 3,
-          createdAt: '1900-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        }
-      ];
-
       service.getAllArtists().subscribe(x => {
         expect(x.length).toEqual(2);
         expect(x).toEqual(dummyArtists);
@@ -54,35 +114,6 @@ describe('MediaService', () => {
 
   describe('#getAlbumsForArtist', () => {
     it('should only returns album for the requested artist>', done => {
-      const dummyAlbums = [
-        {
-          id: 1,
-          artistId: 1,
-          name: 'Album1',
-          cover: 'cover',
-          coverSmall: 'cover',
-          coverMedium: 'cover',
-          coverBig: 'cover',
-          coverXL: 'cover',
-          createdAt: 'cover',
-          updatedAt: 'cover',
-          isFavorite: false
-        },
-        {
-          id: 2,
-          artistId: 2,
-          name: 'Album2',
-          cover: 'cover',
-          coverSmall: 'cover',
-          coverMedium: 'cover',
-          coverBig: 'cover',
-          coverXL: 'cover',
-          createdAt: 'cover',
-          updatedAt: 'cover',
-          isFavorite: false
-        }
-      ];
-
       service.getAlbumsForArtist(2).subscribe(x => {
         expect(x.length).toEqual(1);
         done();
@@ -93,35 +124,6 @@ describe('MediaService', () => {
       req.flush(dummyAlbums);
     });
     it('should returns nothing, if no entry was found>', done => {
-      const dummyAlbums = [
-        {
-          id: 1,
-          artistId: 1,
-          name: 'Album1',
-          cover: 'cover',
-          coverSmall: 'cover',
-          coverMedium: 'cover',
-          coverBig: 'cover',
-          coverXL: 'cover',
-          createdAt: 'cover',
-          updatedAt: 'cover',
-          isFavorite: false
-        },
-        {
-          id: 2,
-          artistId: 2,
-          name: 'Album2',
-          cover: 'cover',
-          coverSmall: 'cover',
-          coverMedium: 'cover',
-          coverBig: 'cover',
-          coverXL: 'cover',
-          createdAt: 'cover',
-          updatedAt: 'cover',
-          isFavorite: false
-        }
-      ];
-
       service.getAlbumsForArtist(3).subscribe(x => {
         expect(x.length).toEqual(0);
         done();
@@ -135,29 +137,6 @@ describe('MediaService', () => {
 
   describe('#getTracksForAlbum ', () => {
     it('returns nothing, if no entry was found', done => {
-      const dummyTracks = [
-        {
-          id: 1,
-          albumId: 2,
-          artistId: 3,
-          title: 'the Title',
-          duration: 100,
-          createdAt: '1990-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        },
-        {
-          id: 2,
-          albumId: 2,
-          artistId: 3,
-          title: 'the Title2',
-          duration: 100,
-          createdAt: '1990-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        }
-      ];
-
       service.getTracksForAlbum(3).subscribe(x => {
         expect(x.length).toEqual(0);
         done();
@@ -169,39 +148,6 @@ describe('MediaService', () => {
     });
 
     it('returns only entries for the required album', done => {
-      const dummyTracks = [
-        {
-          id: 1,
-          albumId: 2,
-          artistId: 3,
-          title: 'the Title',
-          duration: 100,
-          createdAt: '1990-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        },
-        {
-          id: 2,
-          albumId: 77,
-          artistId: 3,
-          title: 'the Title2',
-          duration: 100,
-          createdAt: '1990-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        },
-        {
-          id: 3,
-          albumId: 77,
-          artistId: 3,
-          title: 'the Title3',
-          duration: 100,
-          createdAt: '1990-01-01',
-          updatedAt: '1990-01-01',
-          isFavorite: false
-        }
-      ];
-
       service.getTracksForAlbum(77).subscribe(x => {
         expect(x.length).toEqual(2);
         done();
--- src/app/shared/services/news.service.spec.ts
+++ src/app/shared/services/news.service.spec.ts
@@ -11,6 +11,39 @@ describe('NewsService', () => {
   let injector: TestBed;
   let httpMock: HttpTestingController;
   let service: NewsService;
+  const dummyArticles = [
+    {
+      title: 'Heizkosten: Gaspreise sinken ',
+      description: 'Der Gasmarkt hat sich grundlegend gewandelt.',
+      link:
+        'http://www.spiegel.de/wirtschaft/service/gaspreise-sinken-auf-tiefsten-stand-seit-zwoelf-jahren-a-1163414.html#ref=rss',
+      createdAt: '2017-08-21T06:20:30.770Z',
+      updatedAt: '2017-08-21T06:20:30.770Z',
+      id: 1,
+      isFavorite: false
+    },
+    {
+      title: 'Ford zahlt 10 Millionen Dollar wegen Diskriminierungsklage',
+      description: 'In Ford-Werken in Chicago wurden Mitarbeiter belästigt ',
+      link:
+        'http://www.spiegel.de/wirtschaft/unternehmen/ford-zahlt-10-millionen-dollar-wegen-diskriminierungsklage-a-1163413.html#ref=rss',
+      createdAt: '2017-08-21T06:20:30.772Z',
+      updatedAt: '2017-08-21T06:20:30.772Z',
+      id: 2,
+      isFavorite: false
+    },
+    {
+      title: 'Nachwuchs für US-Sänger: Jetzt kommts Thicke',
+      description: 'Es ist nicht lange her, da verlor Robin Thicke ...',
+      link:
+        'http://www.spiegel.de/panorama/leute/robin-thicke-freundin-von-us-saenger-erwartet-ein-kind-a-1163416.html#ref=rss',
+      createdAt: '2017-08-21T06:20:30.773Z',
+      updatedAt: '2017-08-21T06:20:30.773Z',
+      id: 3,
+      isFavorite: false
+    }
+  ];
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -22,42 +55,31 @@ describe('NewsService', () => {
   });
 
   it('should return all articles', done => {
-    const dummyArticles = [
-      {
-        title: 'Heizkosten: Gaspreise sinken ',
-        description: 'Der Gasmarkt hat sich grundlegend gewandelt.',
-        link:
-          'http://www.spiegel.de/wirtschaft/service/gaspreise-sinken-auf-tiefsten-stand-seit-zwoelf-jahren-a-1163414.html#ref=rss',
-        createdAt: '2017-08-21T06:20:30.770Z',
-        updatedAt: '2017-08-21T06:20:30.770Z',
-        id: 1,
-        isFavorite: false
-      },
-      {
-        title: 'Ford zahlt 10 Millionen Dollar wegen Diskriminierungsklage',
-        description: 'In Ford-Werken in Chicago wurden Mitarbeiter belästigt ',
-        link:
-          'http://www.spiegel.de/wirtschaft/unternehmen/ford-zahlt-10-millionen-dollar-wegen-diskriminierungsklage-a-1163413.html#ref=rss',
-        createdAt: '2017-08-21T06:20:30.772Z',
-        updatedAt: '2017-08-21T06:20:30.772Z',
-        id: 2,
-        isFavorite: false
-      },
-      {
-        title: 'Nachwuchs für US-Sänger: Jetzt kommts Thicke',
-        description: 'Es ist nicht lange her, da verlor Robin Thicke ...',
-        link:
-          'http://www.spiegel.de/panorama/leute/robin-thicke-freundin-von-us-saenger-erwartet-ein-kind-a-1163416.html#ref=rss',
-        createdAt: '2017-08-21T06:20:30.773Z',
-        updatedAt: '2017-08-21T06:20:30.773Z',
-        id: 3,
-        isFavorite: false
-      }
-    ];
-
     service.getArticles().subscribe(articles => {
       expect(articles.length).toEqual(3);
       expect(articles).toEqual(dummyArticles);
+      done();
+    });
+
+    const req = httpMock.expectOne(`${environment.baseUrl}news`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyArticles);
+  });
+
+  it('should only return entries which match the search', done => {
+    service.getArticlesWith('thick').subscribe(articles => {
+      expect(articles.length).toEqual(1);
+      done();
+    });
+
+    const req = httpMock.expectOne(`${environment.baseUrl}news`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyArticles);
+  });
+
+  test('should return empty array if there are no hits', done => {
+    service.getArticlesWith('thickkkkk').subscribe(articles => {
+      expect(articles.length).toEqual(0);
       done();
     });
 
