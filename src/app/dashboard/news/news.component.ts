4b6f1c2fd49d6a41eeb40aab3e33e4bcd14b4af6
flwa6204 <florian.warnke@esolutions.de>
2020-01-30 15:12:41 +0100

    refactor: make the article component to a presentation component

--- src/app/dashboard/news/news.component.ts
+++ src/app/dashboard/news/news.component.ts
@@ -3,6 +3,8 @@ import { Article } from '../../shared/models/article.model';
 import { NewsService } from '../../shared/services/news.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
+import { SearchResultType } from '../../shared/models/search-result-type.enum';
 
 @Component({
   selector: 'db-news',
@@ -14,7 +16,11 @@ export class NewsComponent implements OnInit {
   public articles$: Observable<Article[]>;
   public isFullscreen: boolean;
 
-  constructor(private newsService: NewsService, private router: Router) {
+  constructor(
+    private newsService: NewsService,
+    private router: Router,
+    private userService: UserService
+  ) {
     this.articles$ = new Observable<Article[]>();
     this.isFullscreen = this.router.url.indexOf('/news') !== -1;
   }
@@ -30,5 +36,10 @@ export class NewsComponent implements OnInit {
       this.router.navigate(['news']);
       this.isFullscreen = true;
     }
+  }
+
+  public onToggleArticleFavorite(article: Article): void {
+    this.userService.toggleFavorite(article.id, SearchResultType.Article);
+    article.isFavorite = !article.isFavorite;
   }
 }
